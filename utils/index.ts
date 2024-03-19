017fb58943d5ea99af4027db5b2b546751cb4c34
李云富 <liyunfu1998@qq.com>
2024-03-19 14:20:20 +0800

    feat: 实现工具函数

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,8 @@
 export { default as getTotal } from "./getTotal";
 export { default as existItem } from "./existItem";
+export { default as formatNumber } from "./formatNumber";
+export { default as truncate } from "./truncate";
+
+export * from "./validation";
+export * from "./constants";
+export * from "./exchange";
